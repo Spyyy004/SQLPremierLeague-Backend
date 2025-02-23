91415c071f85d0e20e6f3a371a7ad83a697f2dcf
AyushPawar004 <ayush.pawar@mosaicwellness.in>
2025-02-23 12:41:11 +0530

    cors changes

--- auth.py
+++ auth.py
@@ -8,7 +8,7 @@ import secrets
 
 # Initialize Flask app
 app = Flask(__name__)
-CORS(app, supports_credentials=True,origins=["http://localhost:3000","https://sqlpremierleague-frontend.onrender.com"])
+CORS(app, supports_credentials=True)
 app.config["JWT_SECRET_KEY"] = "supersecretkey"  # Change this in production!
 
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]  # ✅ Look for JWTs in cookies instead of headers
